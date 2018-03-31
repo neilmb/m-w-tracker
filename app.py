f077f7a1bf8e35dbe0103ac206d822c399526be1
Neil Martinsen-Burrell <neil@moz.com>
2018-03-30 20:29:20 -0500

    First app commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, escape, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    app_str = escape(repr(app))
+    return render_template('index.html', title='Home', parameter=app_str)
+
+#import models
