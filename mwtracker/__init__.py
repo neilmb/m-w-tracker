24ab056d725595f43b1c6afa558fb845f0a880a3
Neil MartinsenBurrell <neil@moz.com>
2018-07-01 13:56:12 -0500

    Include CSRF at the app level

--- mwtracker/__init__.py
+++ mwtracker/__init__.py
@@ -2,8 +2,10 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
